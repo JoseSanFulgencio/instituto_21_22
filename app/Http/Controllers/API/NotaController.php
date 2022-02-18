307cfc810fc7601873a17c7cd9f27adcff9485f6
alumno <alumno@vm1>
2022-02-18 01:45:17 +0100

    Repaso 2º trimestre

--- app/Http/Controllers/API/NotaController.php
+++ app/Http/Controllers/API/NotaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\NotaResource;
 use App\Models\Nota;
 use Illuminate\Http\Request;
 
@@ -15,7 +16,7 @@ class NotaController extends Controller
      */
     public function index()
     {
-        //
+        return NotaResource::collection(Nota::paginate(20));
     }
 
     /**
