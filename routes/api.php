9f5a5f84caa1e24b66b4b361f1a76d5be9eb1976
alumno <alumno@vm1>
2022-02-21 09:49:58 +0100

    Examen de repaso ejercicio 1

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ use App\Http\Controllers\API\MatriculaController;
 use App\Http\Controllers\API\PeriodoLectivoController;
 use App\Http\Controllers\API\MateriaMatriculadaController;
 use App\Http\Controllers\API\NotaController;
+use App\Http\Controllers\API\CursoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Psr\Http\Message\ServerRequestInterface;
@@ -68,6 +69,9 @@ Route::apiResource('matriculas', MatriculaController::class)
     ->middleware('auth:sanctum');
 
 Route::apiResource('notas', NotaController::class)
+    ->middleware('auth:sanctum');
+
+Route::apiResource('cursos', CursoController::class)
     ->middleware('auth:sanctum');
 
 Route::apiResource('niveles', NivelController::class)
